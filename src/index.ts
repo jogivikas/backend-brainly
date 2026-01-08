d34b489bb478544d6ae74373cc49d6f9c5af462c
viksuuu... <147147366+jogivikas@users.noreply.github.com>
2026-01-08 15:35:13 +0530

    complete backend

---
+++ src/index.ts
@@ -0,0 +1,182 @@
+import express from "express";
+import { random } from "./utils.js";
+import jwt from "jsonwebtoken";
+import { ContentModel, LinkModel, UserModel } from "./db.js";
+import { JWT_PASSWORD } from "./config.js";
+import { userMiddleware } from "./middleware.js";
+import cors from "cors";
+import { z } from "zod";
+import bcrypt from "bcrypt";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/v1/signup", async (req, res) => {
+  const requiredBody = z.object({
+    username: z.string().min(3).max(10),
+    password: z.string().min(3).max(20)
+  })
+
+  const parsedDataWithSuccess = requiredBody.safeParse(req.body);
+
+  if (!parsedDataWithSuccess.success) {
+    res.json({
+      message: "Incorrect format",
+      error: parsedDataWithSuccess.error
+    })
+    return
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(parsedDataWithSuccess.data.password, 5);
+    await UserModel.create({
+      username: parsedDataWithSuccess.data.username,
+      password: hashedPassword,
+    });
+
+    res.json({
+      message: "User signed up",
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "User already exists",
+    });
+  }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const existingUser = await UserModel.findOne({
+    username
+  });
+  if (existingUser) {
+    // @ts-ignore
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (passwordMatch) {
+      const token = jwt.sign({
+        id: existingUser._id
+      }, JWT_PASSWORD);
+
+      res.json({
+        token
+      })
+    } else {
+      res.status(403).json({
+        message: "Incorrect credentials"
+      })
+    }
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials"
+    })
+  }
+});
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+  const link = req.body.link;
+  const type = req.body.type;
+  await ContentModel.create({
+    link,
+    type,
+    title: req.body.title,
+    // @ts-ignore
+    userId: req.userId,
+    tags: [],
+  });
+
+  res.json({
+    message: "Content added",
+  });
+});
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId = req.userId;
+  const content = await ContentModel.find({
+    userId: userId,
+  }).populate("userId", "username");
+  res.json({
+    content,
+  });
+});
+
+
+
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+  const share = req.body.share;
+  if (share) {
+    const existingLink = await LinkModel.findOne({
+      // @ts-ignore
+      userId: req.userId,
+    });
+
+    if (existingLink) {
+      res.json({
+        hash: existingLink.hash,
+      });
+      return;
+    }
+    const hash = random(10);
+    await LinkModel.create({
+      // @ts-ignore
+      userId: req.userId,
+      hash: hash,
+    });
+
+    res.json({
+      hash,
+    });
+  } else {
+    await LinkModel.deleteOne({
+      // @ts-ignore
+      userId: req.userId,
+    });
+
+    res.json({
+      message: "Removed link",
+    });
+  }
+});
+
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+  const hash = req.params.shareLink;
+
+  const link = await LinkModel.findOne({
+    hash,
+  });
+
+  if (!link) {
+    res.status(411).json({
+      message: "Sorry incorrect input",
+    });
+    return;
+  }
+  // userId
+  const content = await ContentModel.find({
+    userId: link.userId,
+  });
+
+  console.log(link);
+  const user = await UserModel.findOne({
+    _id: link.userId,
+  });
+
+  if (!user) {
+    res.status(411).json({
+      message: "user not found, error should ideally not happen",
+    });
+    return;
+  }
+
+  res.json({
+    username: user.username,
+    content: content,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config.js";
+import { escape } from "querystring";
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers["authorization"];
+  const decoded = jwt.verify(header as string, JWT_PASSWORD);
+  try {
+    if (decoded) {
+      if (typeof decoded === "string") {
+        res.status(403).json({
+          message: "You are not logged in"
+        })
+        return;
+      }
+      req.userId = (decoded as any).id;
+      next();
+    } else {
+      res.status(403).json({
+        message: "You are not logged in"
+      })
+    }
+  } catch (e) {
+    res.status(403).json({
+      message: "You are not logged in"
+    })
+  }
+
+};
